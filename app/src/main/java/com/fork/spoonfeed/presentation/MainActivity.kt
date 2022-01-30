8398cba831ca1727eda0b23b7a67d049f69f1a24
DaBin <84564695+dabinKim-0318@users.noreply.github.com>
2022-01-30 18:02:42 +0900

    [ADD] #1 - 폴더 생성

--- app/src/main/java/com/fork/spoonfeed/presentation/MainActivity.kt
+++ app/src/main/java/com/fork/spoonfeed/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.fork.spoonfeed
+package com.fork.spoonfeed.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.fork.spoonfeed.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
