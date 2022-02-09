b53907d5d8829e5eff62e0a088d9ebc6738a54cc
DaBin <84564695+dabinKim-0318@users.noreply.github.com>
2022-02-09 20:33:27 +0900

    [ADD] #13 - 관심정책 Activity 추가

---
+++ app/src/main/java/com/fork/spoonfeed/presentation/ui/mypage/view/InterastedPolicyActivity.kt
@@ -0,0 +1,11 @@
+package com.fork.spoonfeed.presentation.ui.mypage.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InterastedPolicyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_interasted_policy)
+    }
+}
