e54b0da774e1100a3e3d4f30b4bfb690e8a46476
DaBin <84564695+dabinKim-0318@users.noreply.github.com>
2022-02-09 11:29:28 +0900

    [ADD] #10 - 카카오로그인 라이브러리 세팅

--- app/src/main/java/com/fork/spoonfeed/SpoonFeedApplication.kt
+++ app/src/main/java/com/fork/spoonfeed/SpoonFeedApplication.kt
@@ -1,7 +1,19 @@
 package com.fork.spoonfeed
 
 import android.app.Application
+import android.content.Context
+import com.kakao.sdk.common.KakaoSdk
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class SpoonFeedApplication : Application()
+class SpoonFeedApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        appContext = this
+        KakaoSdk.init(this, getString(R.string.kakao_native_key))
+    }
+
+    companion object {
+        var appContext: Context? = null
+    }
+}
