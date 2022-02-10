1b9d885cf6252f4f82b9d63505b523639abfc94a
DaBin <84564695+dabinKim-0318@users.noreply.github.com>
2022-02-10 11:06:04 +0900

    [FEAT] #13 -  정책리스트, 관심있는 정책 살펴보기 뷰로 이동

--- app/src/main/java/com/fork/spoonfeed/presentation/ui/home/view/HomeFragment.kt
+++ app/src/main/java/com/fork/spoonfeed/presentation/ui/home/view/HomeFragment.kt
@@ -1,14 +1,13 @@
 package com.fork.spoonfeed.presentation.ui.home.view
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import com.fork.spoonfeed.R
-import com.fork.spoonfeed.databinding.FragmentCommunityBinding
 import com.fork.spoonfeed.databinding.FragmentHomeBinding
 import com.fork.spoonfeed.presentation.base.BaseViewUtil
+import com.fork.spoonfeed.presentation.ui.policylist.PolicyListActivity
+import com.fork.spoonfeed.presentation.ui.mypage.view.InterastedPolicyActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -20,6 +19,7 @@ class HomeFragment : BaseViewUtil.BaseFragment<FragmentHomeBinding>(R.layout.fra
     }
 
     override fun initView() {
+        initClick()
     }
 
 
@@ -27,7 +27,22 @@ class HomeFragment : BaseViewUtil.BaseFragment<FragmentHomeBinding>(R.layout.fra
         //맞춤정책 뷰로 이동
 
         //청년정책 살펴보기 뷰로 이동
-
+        binding.ivHomeAllBackgroung.setOnClickListener {
+            val intent = Intent(requireContext(), PolicyListActivity::class.java)
+            startActivity(intent)
+        }
+        binding.ivHomeDwellingBackgroung.setOnClickListener {
+            val intent = Intent(requireContext(), PolicyListActivity::class.java)
+            startActivity(intent)
+        }
+        binding.ivHomeFinanceBackgroung.setOnClickListener {
+            val intent = Intent(requireContext(), PolicyListActivity::class.java)
+            startActivity(intent)
+        }
         //관심있는정책 살펴보기 뷰로 이동
+        binding.ivHomeInterastedPolicyMore.setOnClickListener {
+            val intent = Intent(requireContext(), InterastedPolicyActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/fork/spoonfeed/presentation/ui/mypage/view/InterastedPolicyActivity.kt
+++ app/src/main/java/com/fork/spoonfeed/presentation/ui/mypage/view/InterastedPolicyActivity.kt
@@ -2,10 +2,16 @@ package com.fork.spoonfeed.presentation.ui.mypage.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.fork.spoonfeed.R
+import com.fork.spoonfeed.databinding.ActivityInterastedPolicyBinding
+import com.fork.spoonfeed.presentation.base.BaseViewUtil
 
-class InterastedPolicyActivity : AppCompatActivity() {
+class InterastedPolicyActivity : BaseViewUtil.BaseAppCompatActivity<ActivityInterastedPolicyBinding>(R.layout.activity_interasted_policy) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_interasted_policy)
+        initView()
+    }
+
+    override fun initView() {
     }
 }
