e4f67755a379fa3c31dad8572895cb363256fec1
DaBin <84564695+dabinKim-0318@users.noreply.github.com>
2022-02-10 19:16:29 +0900

    [FEAT] #14 - 정책 리스트 레이아웃 구현

--- app/src/main/java/com/fork/spoonfeed/presentation/ui/policylist/PolicyListActivity.kt
+++ app/src/main/java/com/fork/spoonfeed/presentation/ui/policylist/PolicyListActivity.kt
@@ -1,17 +1,43 @@
 package com.fork.spoonfeed.presentation.ui.policylist
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ScrollView
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.fork.spoonfeed.R
-import com.fork.spoonfeed.databinding.ActivityPolicyListBindingImpl
+import com.fork.spoonfeed.databinding.ActivityPolicyListBinding
 import com.fork.spoonfeed.presentation.base.BaseViewUtil
+import com.fork.spoonfeed.presentation.ui.policylist.adapter.PolicyListAdapter
+import com.fork.spoonfeed.presentation.ui.policylist.adapter.PolicyListResponseData
 
-class PolicyListActivity : BaseViewUtil.BaseAppCompatActivity<ActivityPolicyListBindingImpl>(R.layout.activity_policy_list) {
+class PolicyListActivity : BaseViewUtil.BaseAppCompatActivity<ActivityPolicyListBinding>(R.layout.activity_policy_list) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         initView()
     }
 
     override fun initView() {
+        initAdapter(
+            mutableListOf(
+                PolicyListResponseData(1, "주거", "청년 우대 통장", "아이조아아이조아아이조아", "2022.02-0222.02", 2),
+                PolicyListResponseData(1, "주거", "청년 우대 통장", "아이조아아이조아아이조아", "2022.02-0222.02", 2),
+                PolicyListResponseData(1, "주거", "청년 우대 통장", "아이조아아이조아아이조아", "2022.02-0222.02", 2),
+                PolicyListResponseData(1, "주거", "청년 우대 통장", "아이조아아이조아아이조아", "2022.02-0222.02", 2),
+                PolicyListResponseData(1, "주거", "청년 우대 통장", "아이조아아이아이조아아이조아아이조아아이조아아이조아아이조아아이조아아이조아아이조아아이조아아이조아아이조아아이조아아이조아아이조아아이조아아이조아아이조아", "2022.02-0222.02", 2),
+                PolicyListResponseData(1, "주거", "청년 우대 통장", "아이조아아이조아아이조아", "2022.02-0222.02", 2),
+                PolicyListResponseData(1, "주거", "청년 우대 통장", "아이조아아이조아아이조아", "2022.02-0222.02", 2)
+            )
+        )
+    }
+
+    private fun initAdapter(dataList: MutableList<PolicyListResponseData>) {
+        val policyListAdapter = PolicyListAdapter(dataList) { item ->
+            startActivity(Intent(this, DetailInfoActivity::class.java))
+        }
+
+        with(binding) {
+            rvPolicylist.adapter = policyListAdapter
+            binding.rvPolicylist.layoutManager = LinearLayoutManager(this@PolicyListActivity)
+        }
     }
 }
