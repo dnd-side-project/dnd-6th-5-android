f93d4b66c73561a7235757b2ed9b7110a2a71b1c
DaBin <84564695+dabinKim-0318@users.noreply.github.com>
2022-02-10 11:22:40 +0900

    [ADD] #13 - 정책 상세 정보 Activity 추가

---
+++ app/src/main/java/com/fork/spoonfeed/presentation/ui/policylist/DetailInfoActivity.kt
@@ -0,0 +1,17 @@
+package com.fork.spoonfeed.presentation.ui.policylist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.fork.spoonfeed.R
+import com.fork.spoonfeed.databinding.ActivityDetailInfoBinding
+import com.fork.spoonfeed.presentation.base.BaseViewUtil
+
+class DetailInfoActivity : BaseViewUtil.BaseAppCompatActivity<ActivityDetailInfoBinding>(R.layout.activity_detail_info) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        initView()
+    }
+
+    override fun initView() {
+    }
+}
