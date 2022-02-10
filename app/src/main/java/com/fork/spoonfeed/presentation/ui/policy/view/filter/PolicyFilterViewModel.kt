abdd140eca301d9d508a6f0088721dda1c97fc7e
dddooo9 <dlaehdus99@naver.com>
2022-02-11 03:22:09 +0900

    [FEAT] #9 - 정책 필터 2단계 validation 구현

--- app/src/main/java/com/fork/spoonfeed/presentation/ui/policy/view/filter/PolicyFilterViewModel.kt
+++ app/src/main/java/com/fork/spoonfeed/presentation/ui/policy/view/filter/PolicyFilterViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.fork.spoonfeed.domain.model.Age
+import com.fork.spoonfeed.domain.model.CompanySize
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
@@ -15,6 +16,12 @@ class PolicyFilterViewModel @Inject constructor() : ViewModel() {
 
     private val _marriageStatus = MutableLiveData<Boolean>()
     val marriageStatus: LiveData<Boolean> = _marriageStatus
+
+    private val _employmentAvailability = MutableLiveData<Boolean>()
+    val employmentAvailability: LiveData<Boolean> = _employmentAvailability
+
+    private val _companySize = MutableLiveData<CompanySize>()
+    val companySize: LiveData<CompanySize> = _companySize
 
     fun setAge(year: Int? = null, month: Int? = null, day: Int? = null) {
         val newYear = year.let { if (it == 0) null else it ?: _age.value?.year }
@@ -30,6 +37,18 @@ class PolicyFilterViewModel @Inject constructor() : ViewModel() {
 
     fun isLevelOneValid(): Boolean {
         return _age.value?.isValid() == true && _marriageStatus.value != null
+    }
+
+    fun setEmploymentAvailability(availability: Boolean) {
+        _employmentAvailability.value = availability
+    }
+
+    fun setCompanySize(size: CompanySize) {
+        _companySize.value = size
+    }
+
+    fun isLevelTwoValid(): Boolean {
+        return _employmentAvailability.value != null && _companySize.value !== null
     }
 }
 
