ff134e115e0eb5e96404fc891df56c37ac0d0347
DaBin <84564695+dabinKim-0318@users.noreply.github.com>
2022-02-12 17:00:03 +0900

    [DEL] #13 - 필요없는 주석 삭제

--- app/src/main/java/com/fork/spoonfeed/presentation/ui/home/view/HomeFragment.kt
+++ app/src/main/java/com/fork/spoonfeed/presentation/ui/home/view/HomeFragment.kt
@@ -6,8 +6,8 @@ import android.view.View
 import com.fork.spoonfeed.R
 import com.fork.spoonfeed.databinding.FragmentHomeBinding
 import com.fork.spoonfeed.presentation.base.BaseViewUtil
-import com.fork.spoonfeed.presentation.ui.policylist.PolicyListActivity
 import com.fork.spoonfeed.presentation.ui.mypage.view.InterastedPolicyActivity
+import com.fork.spoonfeed.presentation.ui.policylist.PolicyListActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -24,25 +24,25 @@ class HomeFragment : BaseViewUtil.BaseFragment<FragmentHomeBinding>(R.layout.fra
 
 
     private fun initClick() {
-        //맞춤정책 뷰로 이동
+        with(binding) {
+            //맞춤정책 뷰로 이동
 
-        //청년정책 살펴보기 뷰로 이동
-        binding.ivHomeAllBackgroung.setOnClickListener {
-            val intent = Intent(requireContext(), PolicyListActivity::class.java)
-            startActivity(intent)
-        }
-        binding.ivHomeDwellingBackgroung.setOnClickListener {
-            val intent = Intent(requireContext(), PolicyListActivity::class.java)
-            startActivity(intent)
-        }
-        binding.ivHomeFinanceBackgroung.setOnClickListener {
-            val intent = Intent(requireContext(), PolicyListActivity::class.java)
-            startActivity(intent)
-        }
-        //관심있는정책 살펴보기 뷰로 이동
-        binding.ivHomeInterastedPolicyMore.setOnClickListener {
-            val intent = Intent(requireContext(), InterastedPolicyActivity::class.java)
-            startActivity(intent)
+            ivHomeAllBackgroung.setOnClickListener {
+                val intent = Intent(requireContext(), PolicyListActivity::class.java)
+                startActivity(intent)
+            }
+            ivHomeDwellingBackgroung.setOnClickListener {
+                val intent = Intent(requireContext(), PolicyListActivity::class.java)
+                startActivity(intent)
+            }
+            ivHomeFinanceBackgroung.setOnClickListener {
+                val intent = Intent(requireContext(), PolicyListActivity::class.java)
+                startActivity(intent)
+            }
+            ivHomeInterastedPolicyMore.setOnClickListener {
+                val intent = Intent(requireContext(), InterastedPolicyActivity::class.java)
+                startActivity(intent)
+            }
         }
     }
 }
