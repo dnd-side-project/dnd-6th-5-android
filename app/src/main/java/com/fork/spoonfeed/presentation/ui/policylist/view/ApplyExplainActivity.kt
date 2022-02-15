5a4fd25cabd291cc9a91c0e069f5834cd2b390c5
DaBin <84564695+dabinKim-0318@users.noreply.github.com>
2022-02-15 15:17:55 +0900

    [FEAT] #19 - 찜하기 버튼 구현 및 신청자격, 지원내용 뷰로 이동

--- app/src/main/java/com/fork/spoonfeed/presentation/ui/policylist/view/ApplyExplainActivity.kt
+++ app/src/main/java/com/fork/spoonfeed/presentation/ui/policylist/view/ApplyExplainActivity.kt
@@ -2,6 +2,7 @@ package com.fork.spoonfeed.presentation.ui.policylist.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.fork.spoonfeed.R
 
 class ApplyExplainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/fork/spoonfeed/presentation/ui/policylist/view/DetailInfoActivity.kt
+++ app/src/main/java/com/fork/spoonfeed/presentation/ui/policylist/view/DetailInfoActivity.kt
@@ -1,19 +1,64 @@
 package com.fork.spoonfeed.presentation.ui.policylist.view
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.activity.viewModels
 import com.fork.spoonfeed.R
 import com.fork.spoonfeed.databinding.ActivityDetailInfoBinding
 import com.fork.spoonfeed.presentation.base.BaseViewUtil
+import com.fork.spoonfeed.presentation.ui.policylist.viewmodel.DetailInfoViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class DetailInfoActivity : BaseViewUtil.BaseAppCompatActivity<ActivityDetailInfoBinding>(R.layout.activity_detail_info) {
+    private val detailInfoViewModel: DetailInfoViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding.detailInfoViewModel = detailInfoViewModel
+        binding.lifecycleOwner = this
         initView()
     }
 
     override fun initView() {
+        initClick()
+        setLikeBtn()
+    }
+
+    private fun initClick() {
+        with(binding) {
+            ivDetailInfoLike.setOnClickListener {
+                ivDetailInfoLike.toggle()
+                ivDetailInfoLikeBottom.toggle()
+            }
+
+            ivDetailInfoLikeBottom.setOnClickListener {
+                ivDetailInfoLikeBottom.toggle()
+                ivDetailInfoLike.toggle()
+            }
+
+            ivDetailInfoApplyQualifications.setOnClickListener {
+                val intent = Intent(this@DetailInfoActivity, ApplyQualificationActivity::class.java)
+                startActivity(intent)
+            }
+
+            ivDetailInfoApplyExplain.setOnClickListener {
+                val intent = Intent(this@DetailInfoActivity, ApplyExplainActivity::class.java)
+                startActivity(intent)
+            }
+        }
+    }
+
+    private fun setLikeBtn() {
+        if (detailInfoViewModel.isLikeClicked.value ?: false) {
+            with(binding) {
+                ivDetailInfoLike.isChecked = true
+                ivDetailInfoLikeBottom.isChecked = true
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        ///좋아요 버튼 서버통신
     }
 }
