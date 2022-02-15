cc53e279e03a29e20f052aac4fcdaf120ddff59b
DaBin <84564695+dabinKim-0318@users.noreply.github.com>
2022-02-15 14:27:05 +0900

    [ADD] #19 - 신청자격, 지원내용 Activity 추가

---
+++ app/src/main/java/com/fork/spoonfeed/presentation/ui/policylist/view/ApplyExplainActivity.kt
@@ -0,0 +1,11 @@
+package com.fork.spoonfeed.presentation.ui.policylist.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ApplyExplainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_apply_explain)
+    }
+}
---
+++ app/src/main/java/com/fork/spoonfeed/presentation/ui/policylist/view/ApplyQualificationActivity.kt
@@ -0,0 +1,12 @@
+package com.fork.spoonfeed.presentation.ui.policylist.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.fork.spoonfeed.R
+
+class ApplyQualificationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_apply_qualification)
+    }
+}
