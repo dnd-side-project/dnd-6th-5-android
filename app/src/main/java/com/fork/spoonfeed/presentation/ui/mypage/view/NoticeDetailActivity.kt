e85313ad0529f99348d43f04a8e9e65d5f8068e1
DaBin <84564695+dabinKim-0318@users.noreply.github.com>
2022-02-17 20:14:29 +0900

    [ADD] #26 - 공지사항 상세 View 레이아웃 구현

--- app/src/main/java/com/fork/spoonfeed/presentation/ui/mypage/view/NoticeDetailActivity.kt
+++ app/src/main/java/com/fork/spoonfeed/presentation/ui/mypage/view/NoticeDetailActivity.kt
@@ -3,10 +3,23 @@ package com.fork.spoonfeed.presentation.ui.mypage.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.fork.spoonfeed.R
+import com.fork.spoonfeed.databinding.ActivityDetailInfoBinding
+import com.fork.spoonfeed.databinding.ActivityNoticeDetailBinding
+import com.fork.spoonfeed.presentation.base.BaseViewUtil
 
-class NoticeDetailActivity : AppCompatActivity() {
+class NoticeDetailActivity : BaseViewUtil.BaseAppCompatActivity<ActivityNoticeDetailBinding>(R.layout.activity_notice_detail) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_notice_detail)
+        initView()
+    }
+
+    override fun initView() {
+        setBackBtnClickListener()
+    }
+
+    private fun setBackBtnClickListener() {
+        binding.ivNoticeDetailBack.setOnClickListener {
+            finish()
+        }
     }
 }
