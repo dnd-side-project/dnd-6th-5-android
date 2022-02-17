30cc9950b4b081b36e49346aeac1dbad97b50bf0
DaBin <84564695+dabinKim-0318@users.noreply.github.com>
2022-02-17 17:01:51 +0900

    [ADD] #26 - 내 글 관리 view 생성 및 TabLayout, ViewPager 연결

--- app/src/main/java/com/fork/spoonfeed/presentation/ui/mypage/view/MyPostManagementActivity.kt
+++ app/src/main/java/com/fork/spoonfeed/presentation/ui/mypage/view/MyPostManagementActivity.kt
@@ -1,17 +1,42 @@
 package com.fork.spoonfeed.presentation.ui.mypage.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.fork.spoonfeed.R
 import com.fork.spoonfeed.databinding.ActivityMyPostManagementBinding
 import com.fork.spoonfeed.presentation.base.BaseViewUtil
+import com.fork.spoonfeed.presentation.ui.community.adapter.TabLayoutAdapter
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MyPostManagementActivity : BaseViewUtil.BaseAppCompatActivity<ActivityMyPostManagementBinding>(R.layout.activity_my_post_management) {
+    private lateinit var myPostManagementAdapter: TabLayoutAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         initView()
     }
 
     override fun initView() {
+        initTabLayoutAdapter()
+        initTabLayout()
+        setBackBtnClickListener()
+    }
+
+    private fun initTabLayoutAdapter() {
+        val fragmentList = listOf(MyPostManagementPostFragment(), MyPostManagementCommentFragment())
+        myPostManagementAdapter = TabLayoutAdapter(this)
+        myPostManagementAdapter.fragments.addAll(fragmentList)
+        binding.vpMypostmanagement.adapter = myPostManagementAdapter
+    }
+
+    private fun initTabLayout() {
+        val tabLable = listOf("게시글", "댓글")
+        TabLayoutMediator(binding.tlMypostmanagement, binding.vpMypostmanagement) { tab, position ->
+            tab.text = tabLable[position]
+        }.attach()
+    }
+
+    private fun setBackBtnClickListener() {
+        binding.ivMypostmanagementBack.setOnClickListener {
+            finish()
+        }
     }
 }
