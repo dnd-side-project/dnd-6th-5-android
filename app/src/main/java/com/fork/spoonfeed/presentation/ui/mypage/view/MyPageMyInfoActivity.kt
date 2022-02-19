886ae3ec027bb8164a8c9b975ca2078d0d6ba6db
dddooo9 <dlaehdus99@naver.com>
2022-02-19 17:32:11 +0900

    [ADD] #31 - 마이페이지 내 정보 페이지 레이아웃 구성

---
+++ app/src/main/java/com/fork/spoonfeed/presentation/ui/mypage/view/MyPageMyInfoActivity.kt
@@ -0,0 +1,32 @@
+package com.fork.spoonfeed.presentation.ui.mypage.view
+
+import android.os.Bundle
+import android.text.Editable
+import com.fork.spoonfeed.R
+import com.fork.spoonfeed.databinding.ActivityMyPageMyInfoBinding
+import com.fork.spoonfeed.presentation.base.BaseViewUtil
+
+class MyPageMyInfoActivity :
+    BaseViewUtil.BaseAppCompatActivity<ActivityMyPageMyInfoBinding>(R.layout.activity_my_page_my_info) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        initView()
+    }
+
+    override fun initView() {
+        setOnClickListener()
+    }
+
+    private fun setOnClickListener() {
+        binding.mtMypageMyInfoUpdateTitle.setNavigationOnClickListener {
+            finish()
+        }
+        binding.ivMypageMyInfoUpdateNameClear.setOnClickListener {
+            binding.etMypageMyInfoUpdateName.setText("")
+        }
+        binding.mbMypageMyInfoUpdate.setOnClickListener {
+            finish()
+        }
+    }
+}
