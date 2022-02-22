d53e0d4bef622d83326145e20b0530b2f737dd6a
DaBin <84564695+dabinKim-0318@users.noreply.github.com>
2022-02-22 20:04:10 +0900

    [ADD] #46 - 정책 상세 페이지 API 뷰와 모델 연결

--- app/src/main/java/com/fork/spoonfeed/data/remote/datasource/PolicyDataSourceImpl.kt
+++ app/src/main/java/com/fork/spoonfeed/data/remote/datasource/PolicyDataSourceImpl.kt
@@ -2,13 +2,18 @@ package com.fork.spoonfeed.data.remote.datasource
 
 import com.fork.spoonfeed.data.remote.api.policy.PolicyService
 import com.fork.spoonfeed.data.remote.model.policy.ResponsePolicyAllData
+import com.fork.spoonfeed.data.remote.model.policy.ResponsePolicyDetailData
 import javax.inject.Inject
 
-class PolicyDataSourceImpl (
+class PolicyDataSourceImpl @Inject constructor(
     private val policyService: PolicyService
 ) : PolicyDataSource {
 
     override suspend fun getPolicyAll(category: String): ResponsePolicyAllData {
         return policyService.getPolicyAll(category)
+    }
+
+    override suspend fun getPolicyDetail(pk: Int): ResponsePolicyDetailData {
+        return policyService.getPolicyDetail(pk)
     }
 }
