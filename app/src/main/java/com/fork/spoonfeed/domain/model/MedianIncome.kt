d9b2c1dfc6c09ae42acb1450a03b16eaf03e148e
dddooo9 <dlaehdus99@naver.com>
2022-02-23 20:14:18 +0900

    [ADD] #54 - API 요청으로 보낼 값 설정
    - 타입별 value 텍스트 설정

--- app/src/main/java/com/fork/spoonfeed/domain/model/MedianIncome.kt
+++ app/src/main/java/com/fork/spoonfeed/domain/model/MedianIncome.kt
@@ -1,5 +1,7 @@
 package com.fork.spoonfeed.domain.model
 
-enum class MedianIncome {
-    UNDER_30, UNDER_40, UNDER_45, UNDER_50, UNDER_72, UNDER_100, NOTHING, PRIVATE
+enum class MedianIncome(val value: String) {
+    UNDER_30("30% 이하"), UNDER_40("40% 이하"), UNDER_45("45% 이하"),
+    UNDER_50("50% 이하"), UNDER_72("72% 이하"), UNDER_100("100% 이하"),
+    NOTHING("해당없음"), PRIVATE("미공개")
 }
