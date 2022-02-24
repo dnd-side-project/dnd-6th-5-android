655b4930d37e64bf179a835752304ba1044d3a15
dddooo9 <dlaehdus99@naver.com>
2022-02-24 23:10:40 +0900

    [FIX] 댓글작성란이 비었을 때 색상 적용

--- app/src/main/java/com/fork/spoonfeed/presentation/ui/communitypost/view/CommunityPostActivity.kt
+++ app/src/main/java/com/fork/spoonfeed/presentation/ui/communitypost/view/CommunityPostActivity.kt
@@ -55,7 +55,11 @@ class CommunityPostActivity :
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                communityPostViewModel.setCommentInput(s?.toString())
+                if (s.isNullOrEmpty()){
+                    communityPostViewModel.setCommentInput(null)
+                } else {
+                    communityPostViewModel.setCommentInput(s.toString())
+                }
             }
 
             override fun afterTextChanged(s: Editable?) {}
