4ad8eac05d5e37bddbd90cf6e04d0d02db38fb5c
DaBin <84564695+dabinKim-0318@users.noreply.github.com>
2022-03-03 16:27:26 +0900

    [DEL] #60 - 필요없는 데이터 삭제

--- app/src/main/java/com/fork/spoonfeed/presentation/ui/onboarding/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/fork/spoonfeed/presentation/ui/onboarding/viewmodel/LoginViewModel.kt
@@ -1,11 +1,14 @@
 package com.fork.spoonfeed.presentation.ui.onboarding.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.fork.spoonfeed.data.UserData
 import com.fork.spoonfeed.data.remote.model.user.RequestUserNickNameData
+import com.fork.spoonfeed.data.remote.model.user.ResponseUserNickNameData
+import com.fork.spoonfeed.data.remote.model.user.ResponseUserPostData
 import com.fork.spoonfeed.domain.repository.AuthRepository
 import com.fork.spoonfeed.domain.repository.UserRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -23,8 +26,6 @@ class LoginViewModel @Inject constructor(
 
     private val _isKakaoLoginSuccess = MutableLiveData(false)
     val isKakaoLoginSuccess: LiveData<Boolean> = _isKakaoLoginSuccess
-
-    var nickNameText = MutableLiveData<String>()
 
     var _nickNameSetStatus = MutableLiveData(false)
     val nickNameSetStatus: LiveData<Boolean> get() = _nickNameSetStatus
@@ -65,7 +66,7 @@ class LoginViewModel @Inject constructor(
         viewModelScope.launch {
             kotlin.runCatching {
                 val requestUserNickNameData = RequestUserNickNameData(UserData.id.toString(), userNickName)
-                userRepository.patchUserNickName(UserData.accessToken!!, UserData.platform!!, requestUserNickNameData)
+                userRepository.patchUserNickName(UserData.accessToken!!, UserData.platform!!, requestUserNickNameData).data.user.nickname
             }.onSuccess {
                 _nickNameSetStatus.setValue(true)
             }.onFailure {
