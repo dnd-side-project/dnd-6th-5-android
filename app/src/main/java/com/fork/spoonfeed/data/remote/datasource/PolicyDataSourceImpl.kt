790da17b21f4c64d092be931b6030c2e29710ea8
dddooo9 <dlaehdus99@naver.com>
2022-03-12 22:20:03 +0900

    [FEAT] #54 - 맞춤정책 정보 저장 안하는 기능 구현
    - PUT /custom/policy 로 연결

--- app/src/main/java/com/fork/spoonfeed/data/remote/datasource/PolicyDataSourceImpl.kt
+++ app/src/main/java/com/fork/spoonfeed/data/remote/datasource/PolicyDataSourceImpl.kt
@@ -1,9 +1,12 @@
 package com.fork.spoonfeed.data.remote.datasource
 
 import com.fork.spoonfeed.data.remote.api.policy.PolicyService
-import com.fork.spoonfeed.data.remote.model.policy.*
-import com.google.gson.Gson
-import com.google.gson.JsonParser
+import com.fork.spoonfeed.data.remote.model.policy.RequestFilteredPolicy
+import com.fork.spoonfeed.data.remote.model.policy.RequestPolicyLikeData
+import com.fork.spoonfeed.data.remote.model.policy.ResponseFilteredPolicy
+import com.fork.spoonfeed.data.remote.model.policy.ResponsePolicyAllData
+import com.fork.spoonfeed.data.remote.model.policy.ResponsePolicyDetailData
+import com.fork.spoonfeed.data.remote.model.policy.ResponsePolicyLikeData
 import javax.inject.Inject
 
 class PolicyDataSourceImpl @Inject constructor(
@@ -19,15 +22,11 @@ class PolicyDataSourceImpl @Inject constructor(
     }
 
     override suspend fun updateUserInfoAndGetFilteredPolicy(body: RequestFilteredPolicy): ResponseFilteredPolicy {
-        val jsonString = Gson().toJson(body)
-        val jsonObject = JsonParser.parseString(jsonString).asJsonObject
-        return policyService.updateUserInfoAndGetFilteredPolicy(body = jsonObject)
+        return policyService.updateUserInfoAndGetFilteredPolicy(body = body)
     }
 
     override suspend fun getFilteredPolicy(body: RequestFilteredPolicy): ResponseFilteredPolicy {
-        val jsonString = Gson().toJson(body)
-        val jsonObject = JsonParser.parseString(jsonString).asJsonObject
-        return policyService.getFilteredPolicy(body = jsonObject)
+        return policyService.getFilteredPolicy(body = body)
     }
 
     override suspend fun postPolicyLike(body: RequestPolicyLikeData): ResponsePolicyLikeData {
