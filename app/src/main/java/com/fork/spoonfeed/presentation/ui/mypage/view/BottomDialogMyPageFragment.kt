43973ebaf4a4611639df411d7d80cf03f646cc88
DaBin <84564695+dabinKim-0318@users.noreply.github.com>
2022-03-20 01:09:50 +0900

    [ADD] #72 - 커뮤니티, 마이페이지 게시글,댓글 수정/삭제 BottomSheet으로 변경

--- app/src/main/java/com/fork/spoonfeed/presentation/ui/mypage/view/BottomDialogMyPageFragment.kt
+++ app/src/main/java/com/fork/spoonfeed/presentation/ui/mypage/view/BottomDialogMyPageFragment.kt
@@ -5,8 +5,10 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.view.View
+import android.widget.TextView
 import androidx.fragment.app.activityViewModels
 import com.fork.spoonfeed.R
+import com.fork.spoonfeed.data.remote.model.community.ResponsePostData
 import com.fork.spoonfeed.databinding.FragmentBottomDialogMyPageBinding
 import com.fork.spoonfeed.presentation.base.BaseViewUtil
 import com.fork.spoonfeed.presentation.ui.community.view.CommunityFragment
@@ -18,6 +20,24 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class BottomDialogMyPageFragment(private val postId: Int, private val commentId: String, private val type: String) :
     BaseViewUtil.BaseCategoryBottomDialogFragment<FragmentBottomDialogMyPageBinding>(R.layout.fragment_bottom_dialog_my_page) {
+
+    private lateinit var data: ResponsePostData.Data.Comment
+    private lateinit var commentUpdateListener: (ResponsePostData.Data.Comment) -> (Unit)
+    private lateinit var commentDeleteListener: (ResponsePostData.Data.Comment) -> (Unit)
+
+    constructor(
+        postId: Int,
+        commentId: String,
+        type: String,
+        data: ResponsePostData.Data.Comment,
+        commentUpdateListener: (ResponsePostData.Data.Comment) -> (Unit),
+        commentDeleteListener: (ResponsePostData.Data.Comment) -> (Unit)
+    ) : this(postId, commentId, type) {
+        this.data = data
+        this.commentUpdateListener = commentUpdateListener
+        this.commentDeleteListener = commentDeleteListener
+    }
+
     private val myPageViewModel: MyPageViewModel by activityViewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -29,8 +49,10 @@ class BottomDialogMyPageFragment(private val postId: Int, private val commentId:
         setClickListener()
     }
 
+
     private fun setClickListener() {
         with(binding) {
+
             when (type) {
                 MANAGEMENT_POST -> {
                     tvBottomDialogMypageEdit.setOnClickListener {
@@ -42,10 +64,12 @@ class BottomDialogMyPageFragment(private val postId: Int, private val commentId:
                     tvBottomDialogMypageDelete.setOnClickListener {
                         setHandler()
                         myPageViewModel.deleteMyPost(postId)
+                        if (type == COMMUNITY_POST)
+                            (activity as CommunityPostActivity).finish()
                     }
                 }
                 MANAGEMENT_COMMENT -> {
-                    binding.tvBottomDialogMypageEdit.setOnClickListener {
+                    tvBottomDialogMypageEdit.setOnClickListener {
                         startActivity(Intent(context, CommunityPostActivity::class.java).let {
                             it.putExtra(CommunityFragment.POST_PK, postId)
                         })
@@ -56,7 +80,33 @@ class BottomDialogMyPageFragment(private val postId: Int, private val commentId:
                         setHandler()
                     }
                 }
+                COMMUNITY_POST -> {
+                    tvBottomDialogMypageEdit.setOnClickListener {
+                        startActivity(Intent(context, CommunityPostCreateActivity::class.java).let {
+                            it.putExtra(CommunityPostCreateActivity.POST_ID, postId)
+                        })
+                        dialog?.dismiss()
+                    }
+                    tvBottomDialogMypageDelete.setOnClickListener {
+                        setHandler()
+                        myPageViewModel.deleteMyPost(postId)
+                        if (type == COMMUNITY_POST)
+                            (activity as CommunityPostActivity).finish()
+                    }
+                }
+                COMMUNITY_COMMENT -> {
+                    binding.tvBottomDialogMypageEdit.setOnClickListener {
+                        commentUpdateListener(data)
+                        dialog?.dismiss()
+                    }
+                    tvBottomDialogMypageDelete.setOnClickListener {
+                        commentDeleteListener(data)
+                        setHandler()
+                    }
+                }
+
             }
+
             tvBottomDialogMypageClose.setOnClickListener {
                 setHandler()
             }
@@ -70,7 +120,9 @@ class BottomDialogMyPageFragment(private val postId: Int, private val commentId:
 
     companion object {
         const val MANAGEMENT_NO_COMMENT = "NO_COMMENT"
-        const val MANAGEMENT_POST = "POST"
-        const val MANAGEMENT_COMMENT = "COMMENT"
+        const val MANAGEMENT_POST = "MANAGEMENT_POST"
+        const val MANAGEMENT_COMMENT = "MANAGEMENT_COMMENT"
+        const val COMMUNITY_POST = "COMMUNITY_POST"
+        const val COMMUNITY_COMMENT = "COMMUNITY_COMMENT"
     }
 }
