e6cdbe4e1c3bee679a1a79d6a989c620fb6dac62
DaBin <84564695+dabinKim-0318@users.noreply.github.com>
2022-03-21 14:08:39 +0900

    [FEAT] #81 - 상세페이지 하이퍼링크 연결

--- app/src/main/java/com/fork/spoonfeed/presentation/ui/policylist/viewmodel/DetailInfoViewModel.kt
+++ app/src/main/java/com/fork/spoonfeed/presentation/ui/policylist/viewmodel/DetailInfoViewModel.kt
@@ -11,6 +11,7 @@ import com.fork.spoonfeed.data.remote.model.policy.ResponsePolicyDetailData
 import com.fork.spoonfeed.data.remote.model.user.ResponseUserLikePolicyData
 import com.fork.spoonfeed.domain.repository.PolicyRepository
 import com.fork.spoonfeed.domain.repository.UserRepository
+import com.fork.spoonfeed.presentation.ui.policylist.view.DetailInfoActivity
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -37,10 +38,20 @@ class DetailInfoViewModel @Inject constructor(
     val isLikeClicked: LiveData<Boolean>
         get() = _isLikeClicked
 
+    private val _referenceSiteOne = MutableLiveData<String>("")
+    val referenceSiteOne: LiveData<String>
+        get() = _referenceSiteOne
+
+    private val _referenceSiteTwo = MutableLiveData<String>()
+    val referenceSiteTwo: LiveData<String>
+        get() = _referenceSiteTwo
+
     fun getPolicyDetailInfo(id: Int) {
         try {
             viewModelScope.launch {
                 _policyDetailInfo.value = policyRepository.getPolicyDetail(UserData.accessToken!!, UserData.platform!!, id).data.policy
+                _referenceSiteOne.value = _policyDetailInfo.value?.referenceSite1 ?: DetailInfoActivity.REFERENCE_SITE_NOTHING
+                _referenceSiteTwo.value = _policyDetailInfo.value?.referenceSite2 ?: DetailInfoActivity.REFERENCE_SITE_NOTHING
             }
         } catch (e: Exception) {
         }
