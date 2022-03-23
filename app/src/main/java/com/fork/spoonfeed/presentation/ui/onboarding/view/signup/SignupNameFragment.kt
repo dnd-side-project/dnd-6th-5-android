09586f72d2f88667a1fcba84d4c1252e04d5d4e3
DaBin <84564695+dabinKim-0318@users.noreply.github.com>
2022-03-23 12:53:45 +0900

    [MOD] #80 - 온보딩 text 레이아웃 이동막기

--- app/src/main/java/com/fork/spoonfeed/presentation/ui/onboarding/view/signup/SignupNameFragment.kt
+++ app/src/main/java/com/fork/spoonfeed/presentation/ui/onboarding/view/signup/SignupNameFragment.kt
@@ -4,7 +4,6 @@ import android.content.res.ColorStateList
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.activityViewModels
--- app/src/main/java/com/fork/spoonfeed/presentation/util/ViewExt.kt
+++ app/src/main/java/com/fork/spoonfeed/presentation/util/ViewExt.kt
@@ -5,7 +5,6 @@ import android.content.Context
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.DisplayMetrics
-import android.util.Log
 import android.widget.CheckBox
 import android.widget.EditText
 import android.widget.ImageView
