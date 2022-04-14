6df7b8c3014d15e01891932e9970a9cafe3fc404
dddooo9 <dlaehdus99@naver.com>
2022-04-15 00:45:04 +0900

    [REFACTOR] #115 - 신고 Fragment pk를 bundle로 전달

--- app/src/main/java/com/fork/spoonfeed/presentation/ui/communitypost/view/BottomDialogReport.kt
+++ app/src/main/java/com/fork/spoonfeed/presentation/ui/communitypost/view/BottomDialogReport.kt
@@ -1,7 +1,6 @@
 package com.fork.spoonfeed.presentation.ui.communitypost.view
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
@@ -10,10 +9,13 @@ import com.fork.spoonfeed.R
 import com.fork.spoonfeed.databinding.FragmentBottomDialogReportUserBinding
 import com.fork.spoonfeed.presentation.base.BaseViewUtil
 import dagger.hilt.android.AndroidEntryPoint
+import kotlin.properties.Delegates
 
 @AndroidEntryPoint
-class BottomDialogReport(private val postPk: Int) :
+class BottomDialogReport :
     BaseViewUtil.BaseCategoryBottomDialogFragment<FragmentBottomDialogReportUserBinding>(R.layout.fragment_bottom_dialog_report_user) {
+
+    private var postPk by Delegates.notNull<Int>()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -21,8 +23,10 @@ class BottomDialogReport(private val postPk: Int) :
     }
 
     override fun initView() {
-        setReportClickListner()
-
+        arguments?.getInt(CommunityPostActivity.REPORT_POST_PK)?.let {
+            postPk = it
+        }
+        setReportClickListener()
     }
 
     private fun setHandler() {
@@ -30,11 +34,12 @@ class BottomDialogReport(private val postPk: Int) :
         handler.postDelayed({ dialog?.dismiss() }, 140)
     }
 
-    private fun setReportClickListner() {
+    private fun setReportClickListener() {
         binding.tvBottomDialogReportUser.setOnClickListener {
             startActivity(
-                Intent(requireContext(), UserReportReasonActivity::class.java)
-                    .putExtra("postPk", postPk)
+                Intent(requireContext(), UserReportReasonActivity::class.java).apply {
+                    putExtra("postPk", postPk)
+                }
             )
             setHandler()
         }
