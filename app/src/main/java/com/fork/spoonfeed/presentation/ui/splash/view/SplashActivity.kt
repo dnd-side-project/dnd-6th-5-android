99d275115f46095420c5e26b5682f172ec23e3f6
DaBin <84564695+dabinKim-0318@users.noreply.github.com>
2022-05-14 16:40:42 +0900

    [ADD] #124 - 스플래시 풀스크린으로 뜨도록함

--- app/src/main/java/com/fork/spoonfeed/presentation/ui/splash/view/SplashActivity.kt
+++ app/src/main/java/com/fork/spoonfeed/presentation/ui/splash/view/SplashActivity.kt
@@ -4,6 +4,9 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.View
+import android.view.WindowInsets
+import android.view.WindowInsetsController
 import androidx.activity.viewModels
 import com.fork.spoonfeed.R
 import com.fork.spoonfeed.databinding.ActivitySplashBinding
@@ -27,6 +30,27 @@ class SplashActivity :
     override fun initView() {
         viewModel.autoLogin()
         setObserver()
+        setFullScreen()
+    }
+
+    private fun setFullScreen() {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.R) {
+            window.setDecorFitsSystemWindows(false)
+            window.insetsController?.let {
+                it.hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
+                it.systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+            }
+        } else {
+            @Suppress("DEPRECATION")
+            window.decorView.systemUiVisibility = (
+                    View.SYSTEM_UI_FLAG_FULLSCREEN
+                            or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                            or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    )
+        }
     }
 
     private fun setObserver() {
